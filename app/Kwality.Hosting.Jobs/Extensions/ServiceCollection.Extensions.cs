1d7621db5fe89a1c1fd70b13f860acefff9500a4
Kevin De Coninck <kevin.dconinck@gmail.com>
2022-10-30 19:16:23 +0100

    Add: `Job` implementation
    - Add the `Job` implementation.
    - Add the Github actions for building, analyzing and deploying.
    - Prevent output folders, generated by `dotnet` from being committed to
      the GIT repository.

---
+++ app/Kwality.Hosting.Jobs/Extensions/ServiceCollection.Extensions.cs
@@ -0,0 +1,42 @@
+﻿// =====================================================================================================================
+// == LICENSE:       Copyright (c) 2022 Kevin De Coninck
+// ==
+// ==                Permission is hereby granted, free of charge, to any person
+// ==                obtaining a copy of this software and associated documentation
+// ==                files (the "Software"), to deal in the Software without
+// ==                restriction, including without limitation the rights to use,
+// ==                copy, modify, merge, publish, distribute, sublicense, and/or sell
+// ==                copies of the Software, and to permit persons to whom the
+// ==                Software is furnished to do so, subject to the following
+// ==                conditions:
+// ==
+// ==                The above copyright notice and this permission notice shall be
+// ==                included in all copies or substantial portions of the Software.
+// ==
+// ==                THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// ==                EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// ==                OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// ==                NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// ==                HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// ==                WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// ==                FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// ==                OTHER DEALINGS IN THE SOFTWARE.
+// =====================================================================================================================
+namespace Kwality.Hosting.Jobs.Extensions;
+
+using Kwality.Hosting.Jobs.Abstractions;
+
+using Microsoft.Extensions.DependencyInjection;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddJob<TJob, TLock>(this IServiceCollection services)
+        where TJob : Job
+        where TLock : class, ILock
+    {
+        _ = services.AddHostedService<TJob>();
+        _ = services.AddSingleton<ILock, TLock>();
+
+        return services;
+    }
+}
